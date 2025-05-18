93bcf1cd7dc90c7a05a004d886cc593ea3be245a
billjamesevans <88474479+billjamesevans@users.noreply.github.com>
2025-05-18 01:30:36 -0500

    Update metadata

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 setup(
     name="customControl4",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Bill Evans",
+    author_email="billjamesevans@gmail.com",
     description="An improved Python library for interacting with Control4 systems.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/customControl4",
     packages=find_packages(),
@@ -19,7 +19,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Your License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
